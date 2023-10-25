5bbb299f62fd3c4ebcf66927c927cd12d75e3ad3
di4b0lical <79179978+di4b0lical@users.noreply.github.com>
2023-10-24 21:01:12 -0400

    Update NoAdsYt.js

--- NoAdsYt.js
+++ NoAdsYt.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         No ADS - YouTube
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      2.0
 // @description  - Skips all youtube ads - | - undetectable - | - skips ads instantly -
-// @author       GSRHackZ
+// @author       GSRHaX
 // @match        https://www.youtube.com/*
 // @match        https://m.youtube.com/*
 // @icon         https://i.ibb.co/X5f50Cg/Screen-Shot-2021-07-19-at-9-31-54-PM.png
@@ -13,64 +13,44 @@
 let ogVolume=1;
 let pbRate = 1;
 
-setInterval(function(){
+setInterval(()=>{
     if(document.getElementsByClassName("video-stream html5-main-video")[0]!==undefined){
         let ad = document.getElementsByClassName("video-ads ytp-ad-module")[0];
         let vid = document.getElementsByClassName("video-stream html5-main-video")[0];
-        if(ad==undefined){
-            pbRate = vid.playbackRate;
-        }
+        if(ad==undefined){pbRate = vid.playbackRate};
         let closeAble = document.getElementsByClassName("ytp-ad-overlay-close-button");
-        for(let i=0;i<closeAble.length;i++){
-            closeAble[i].click();
-            //console.log("ad banner closed!")
-        }
+        for(let i=0;i<closeAble.length;i++){closeAble[i].click()};
         if(document.getElementsByClassName("style-scope ytd-watch-next-secondary-results-renderer sparkles-light-cta GoogleActiveViewElement")[0]!==undefined){
             let sideAd=document.getElementsByClassName("style-scope ytd-watch-next-secondary-results-renderer sparkles-light-cta GoogleActiveViewElement")[0];
             sideAd.style.display="none";
-            //console.log("side ad removed!")
         }
         if(document.getElementsByClassName("style-scope ytd-item-section-renderer sparkles-light-cta")[0]!==undefined){
-            let sideAd_ = document.getElementsByClassName("style-scope ytd-item-section-renderer sparkles-light-cta")[0];
-            sideAd_.style.display="none";
-            //console.log("side ad removed!")
+            let sideAd_ = document.getElementsByClassName("style-scope ytd-item-section-renderer sparkles-light-cta")[0];sideAd_.style.display="none";
         }
         if(document.getElementsByClassName("ytp-ad-text ytp-ad-skip-button-text")[0]!==undefined){
-            let skipBtn=document.getElementsByClassName("ytp-ad-text ytp-ad-skip-button-text")[0];
-            skipBtn.click();
-            //console.log("skippable ad skipped!")
+            let skipBtn=document.getElementsByClassName("ytp-ad-text ytp-ad-skip-button-text")[0];skipBtn.click();
         }
         if(document.getElementsByClassName("ytp-ad-message-container")[0]!==undefined){
-            let incomingAd=document.getElementsByClassName("ytp-ad-message-container")[0];
-            incomingAd.style.display="none";
-            //console.log("removed incoming ad alert!")
+            let incomingAd=document.getElementsByClassName("ytp-ad-message-container")[0];incomingAd.style.display="none";
         }
         if(document.getElementsByClassName("style-scope ytd-companion-slot-renderer")[0]!==undefined){
             document.getElementsByClassName("style-scope ytd-companion-slot-renderer")[0].remove();
-            //console.log("side ad removed!")
         }
         if(ad!==undefined){
             if(ad.children.length>0){
                 if(document.getElementsByClassName("ytp-ad-text ytp-ad-preview-text")[0]!==undefined){
-                    vid.playbackRate=16;
-                    vid.muted=true;
-                    //console.log("Incrementally skipped unskippable ad!")
+                    vid.playbackRate=16;vid.muted=true;
                 }
             }
         }
         if(document.getElementById("masthead-ad")!==null){
-            let headerAd=document.getElementById("masthead-ad");
-            headerAd.remove();
-            //console.log("Header ad on homepage removed!");
+            let headerAd=document.getElementById("masthead-ad");headerAd.remove();
         }
         if(document.getElementsByTagName("ytd-ad-slot-renderer")[0]!==undefined){
-            let rightSideAd=document.getElementsByTagName("ytd-ad-slot-renderer")[0];
-            rightSideAd.remove();
-            //console.log("Ad above 'next video' on the right side removed!");
+            let rightSideAd=document.getElementsByTagName("ytd-ad-slot-renderer")[0];rightSideAd.remove();
         }
         if(document.getElementsByTagName("ytd-reel-shelf-renderer")[0]!==undefined){
-            let rightSideShorts=document.getElementsByTagName("ytd-reel-shelf-renderer")[0];
-            rightSideShorts.remove();
-            //console.log("Shorts below 'next video' on the right side removed!");
+            let rightSideShorts=document.getElementsByTagName("ytd-reel-shelf-renderer")[0];rightSideShorts.remove();
+        }
     }
-},100)
+},100);
